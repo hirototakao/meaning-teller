241aabe93474c0098366d4e535266e3cac8abb25
hiroto takao <biiiy080121@outlook.com>
2024-03-24 21:31:00 +0900

    Hello

--- meaning-teller-class.js
+++ meaning-teller-class.js
@@ -12,6 +12,7 @@ const app = new bolt.App(
     logLevel: 'debug' 
   });     
 
+<<<<<<< HEAD
   export class meaningTeller { // class for main functions
   }
 
@@ -52,6 +53,9 @@ export class SlackOtherFunctions { //class that is handles processing other than
         }
   });
   } async contentShare(messageRelatedInfo) {
+=======
+  export async function contentShare(messageRelatedInfo) {
+>>>>>>> 2a2ddee (Hello)
     const channelName = messageRelatedInfo.text.match(/share (.+)/i)[1];
     const postedBy = messageRelatedInfo.user;
     const postedChannel = messageRelatedInfo.channel;
@@ -60,6 +64,7 @@ export class SlackOtherFunctions { //class that is handles processing other than
      types: "public_channel,private_channel"
     });
     const channels = conversationLists.channels;
+<<<<<<< HEAD
   try{
     channels.forEach(async (channel) => {
       if(channel.name === channelName) {
@@ -103,3 +108,54 @@ export class SlackOtherFunctions { //class that is handles processing other than
   }
 }  
 
+=======
+    try{
+      channels.forEach(async (channel) => {
+        if(channel.name === channelName) {
+          const channelId = channel.id;
+       const result = await app.client.conversations.history({
+          token: process.env.SLACK_BOT_TOKEN,
+          channel: channelId,
+          inclusive: true,
+          limit: 1
+        });
+        const message = result.messages[0].text;
+        console.log(chalk.blue(`ChannelId: ${channelId}`));
+        console.log(chalk.blue(`Latest message: ${message}`));
+        console.log(chalk.blue(`PostedBy: ${postedBy}`));
+        console.log(chalk.blue(`PostedChannel: ${postedChannel}`));
+        const messages = [`Here's latest message of <#${channelId}|${channel.name}> below.`,
+                          `*Latest message:* ${message}`];
+        for(let i = 0; i < messages.length; i++){
+          await app.client.chat.postMessage({
+            channel: messageRelatedInfo.channel,
+            text: messages[i]
+          });
+        }
+      }
+      });
+      
+  } catch(error) {
+    console.log(chalk.red(error));
+    await say("Latest Message couldn't find from the channel.");
+    await say('Access the channel below to make sure whether this chanel existed latest message or not.');
+    channels.forEach(async (channel) => {
+      if(channel.name === channelName) {
+        const channelId = channel.id;
+        await app.client.chat.postMessage({
+          channel: channelId,
+          text: `<${channelId}|${channelName}>`
+        });
+      }
+   });
+  }
+   }
+
+export async function returnChannelList(callback) {
+  const conversationLists = await app.client.conversations.list({
+    token: process.env.SLACK_BOT_TOKEN,
+    types: "public_channel,private_channel"
+    });
+    callback(conversationLists.channels);
+} 
+>>>>>>> 2a2ddee (Hello)
--- meaning-teller.js
+++ meaning-teller.js
@@ -2,6 +2,7 @@
 import bolt from '@slack/bolt';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
+<<<<<<< HEAD
 dotenv.config();
 
 import fs from 'node:fs';
@@ -9,11 +10,19 @@ import readline from 'readline';
 import { SlackOtherFunctions } from './meaning-teller-class.js';
 const rs = fs.createReadStream('./meaning-data.csv');
 const rl = readline.createInterface({ input: rs});
+=======
+import XlsxPopulate from "./node_modules/xlsx-populate/lib/XlsxPopulate.js";
+import { returnChannelList } from './meaning-teller-class.js';
+import { contentShare } from './meaning-teller-class.js';
+dotenv.config();
+
+>>>>>>> 2a2ddee (Hello)
 const app = new bolt.App(
   { token: process.env.SLACK_BOT_TOKEN, 
     appToken: process.env.SLACK_APP_TOKEN, 
     socketMode: true, 
     logLevel: 'debug' 
+<<<<<<< HEAD
   });
 
   const meaningObject = {}; //definition-stored object
@@ -106,11 +115,102 @@ const app = new bolt.App(
         const updateAt = message.text.match(/update (.+)/i)[1];
         const index = meaningObject[updateAt].index;
         if(updateAt === index) {
+=======
+  });     
+   
+  const meaningObject = {};//Object
+
+  XlsxPopulate.fromFileAsync("./meaning-teller.xlsx").then( async(workbook) => {
+    
+   const sheet = workbook.sheet(0);
+
+    sheet.column("B").width(34);
+    sheet.column("C").width(13);
+    sheet.column("D").width(49);
+    sheet.cell("A1").value("Hello");
+    sheet.cell("B1").value("used when meeting or greeting someone:");
+    sheet.cell("C1").value("greet, greeting");
+    sheet.cell("D1").value("https://dictionary.cambridge.org/dictionary/english/hello");
+
+     return await workbook.toFileAsync("./meaning-teller.xlsx")
+  }).then(() => {
+    console.log("Excel file has been successfully written.");
+  }).catch(error => {
+    console.error(chalk.red("Error occurred:", error));
+  });
+    
+      let userInputWord; // A index and word of posted userInput.
+      let userInputMeaning; // A meaning of posted userInput.
+      let userInputSynonyms; // A synonyms of posted userInput
+      let updateIndex; // A index which is update to
+
+      app.message(/mea (.+)/i, async({message, say}) => {
+        const userInput = await message.text.match(/mea (.+)/i)[1];
+          await say(`Certainly <@${message.user}>, Here's meaning of *${meaningObject[userInput].index}*.`);  
+          await say(`*Meaning:* ${meaningObject[userInput].meaning}`);
+          await say(`*Synonyms:* ${meaningObject[userInput].synonyms}`);
+          await say(`*URL:* ${meaningObject[userInput].URL}`);
+        });   
+  
+     //create-meaning function
+     app.message(/create mea/i, async({say}) => {
+        say('Please type in word, as in *"create word (type in here)"*.');
+     });
+     app.message(/create word (.+)/i, async({message, say}) => {
+        const userInput = message.text.match(/create word (.+)/i)[1];
+        if(userInput ===  meaningObject[userInput].index) {
+          say(`Selected word is already existed!`);
+          return;
+        } else {
+          await say(`Please type in meaning, as in *"create meaning (type in here)"*.`);
+          console.log(chalk.blue(userInput));
+          userInputWord = userInput;
+        }
+      });
+     app.message(/create meaning (.+)/i, async({message, say}) => {
+      const userInput = message.text.match(/create meaning (.+)/i)[1];
+      await say(`Please type in synonyms, as in *"create synonyms (type in here)"*.`);
+      console.log(chalk.blue(userInput));
+      userInputMeaning = userInput; 
+     });
+
+     app.message(/create synonyms (.+)/i, async({message, say}) => {
+        const userInput = message.text.match(/create synonyms (.+)/i)[1];
+        await say(`Please type in URL, as in *"create URL (type in here)"*.`);
+        console.log(chalk.blue(userInput));
+        userInputSynonyms = userInput;
+     });
+
+     app.message(/create synonyms/i, async({say}) => {
+        await say(`Please type in URL, as in *"create URL (type in here)"*.`);
+        console.log(chalk.blue('Not set'));
+        userInputSynonyms = 'Not set';
+     });
+
+      app.message(/create URL (.+)/i, async({message, say}) => {
+        const userInputURL = message.text.match(/create URL (.+)/i)[1];
+        await say(`Meaning-creation processing has been completed successfully.🥳`);
+        console.log(chalk.blue(userInputURL));
+        meaningObject[userInputWord] = {index: userInputWord, meaning: userInputMeaning, synonyms: userInputSynonyms, URL: userInputURL};
+      });
+     
+    app.message(/create URL/i, async({say}) => {
+      const userInputURL = 'Not set';
+      await say(`Meaning-creation processing has been completed successfully.🥳`);
+      console.log(chalk.blue('Not set'));
+      meaningObject[userInputWord] = {index: userInputWord, meaning: userInputMeaning, synonyms: userInputSynonyms, URL: userInputURL};
+    }); 
+
+    app.message(/update (.+)/i, async({message, say}) => {
+      const updateAt = message.text.match(/update (.+)/i)[1];
+        if(updateAt === meaningObject[updateIndex].index){
+>>>>>>> 2a2ddee (Hello)
           await say(`*Meaning:* ${meaningObject[updateAt].meaning}`);
           await say(`*Synonyms:* ${meaningObject[updateAt].synonyms}`);
           await say(`*URL:* ${meaningObject[updateAt].URL}`);
           await say(`Please select to type in item name and detail that is would like to update in ${updateAt}, as in *"update (item name) (detail). "*.`);
           updateIndex = updateAt;
+<<<<<<< HEAD
         } if(updateAt !== index) {
           await say('Not found the selected word.');
           return;
@@ -155,5 +255,102 @@ const app = new bolt.App(
     app.message(/share (.+)/i, async({message}) => {
        OtherFunctions.contentShare(message);
     });  
+=======
+        } else {
+          say('Not found the selected word.');
+          return;
+        }
+   });
+   app.message(/update meaning (.+)/i, async({message, say}) => {
+    const updateInput = await message.text.match(/update meaning (.+)/i)[1];
+    await say(`It successfully updated meaning.😍 Please make sure whether enabled you to check same meaning or not.`);
+    console.log(`UpdatedMeaning: ${updateInput}`);
+    meaningObject[updateIndex].meaning = updateInput;
+    }); 
+
+  app.message(/update synonyms (.+)/i, async({message, say}) => {
+    const updateInput = await message.text.match(/update synonyms (.+)/i)[1];
+    await say(`It successfully updated meaning.😍 Please make sure whether enabled you to check same synonyms or not.`);
+    console.log(`UpdatedSynonyms: ${updateInput}`);
+    meaningObject[updateIndex].synonyms = updateInput;
+    }); 
+
+  app.message(/update URL (.+)/i, async({message, say}) => {
+    const updateInput = await message.text.match(/update URL (.+)/i)[1];
+    await say(`It successfully updated meaning.😍 Please make sure whether enabled you to check same URL or not.`);
+    console.log(`UpdatedURL: ${updateInput}`);
+    meaningObject[updateIndex].URL = updateInput;
+    }); 
+      
+     
+
+       //listing-channel function
+       app.command('/listchannel', async({ack, say}) => {
+          await ack();
+          returnChannelList((channels) => {
+            channels.forEach(async(channelList) => {
+              try{
+              const channelIds = channelList.id;
+              const channelNames = channelList.name;
+               say(`<#${channelIds}|${channelNames}>\nhttps://app.slack.com/client/T05F5GD3ERG/${channelIds}`);
+              } catch(error){
+                console.log(chalk.red(error));
+              }
+            });
+          });
+        });
+
+      //Recommending youtube chanel function 
+    app.message(/recommend youtube learning English/i, async({message, say}) => {
+      await say(`Certainly, <@${message.user}>. Here's the list of recommended youtube channel for learning English.`);
+      await say('<https://www.youtube.com/@Hapaeikaiwapage/videos|Hapa 英会話>');
+      await say("This youtube channel is only Japanese version, but I feel like this channel important to wether how you can use simple word a lot and with different way rather than whether can use new expression or word, so I recommend this channel for those who would like to revise learning method for scratch.");
+      await say('<https://www.youtube.com/channel/UCgeaC4OEk0t54m2hWQtjjIw|Atsueigo>');
+      await say("This channel is kind of channel that managing by people such a hardcore crowd of learning English, so it's extremely professional English and you can learn about learning-method. So if you want to learn more casually or with communication, it's unrecommendable. So I recommend this channel for those who want to learn English in your own country or who would like to plan your learning-method firmly.");
+    });
+    //function that is easy to share content of other channel.
+    app.message(/share (.+)/i, async({message, say}) => {
+        const channelName = message.text.match(/share (.+)/i)[1];
+        const postedBy = message.user;
+        const postedChannel = message.channel;
+        const conversationLists = await app.client.conversations.list({
+         token: process.env.SLACK_BOT_TOKEN,
+         types: "public_channel,private_channel"
+        });
+        const channels = conversationLists.channels;
+      try{
+        channels.forEach(async (channel) => {
+          if(channel.name === channelName) {
+            const channelId = channel.id;
+         const result = await app.client.conversations.history({
+            token: process.env.SLACK_BOT_TOKEN,
+            channel: channelId,
+            inclusive: true,
+            limit: 1
+          });
+          const message = result.messages[0].text;
+          console.log(chalk.blue(`ChannelId: ${channelId}`));
+          console.log(chalk.blue(`Latest message: ${message}`));
+          console.log(chalk.blue(`PostedBy: ${postedBy}`));
+          console.log(chalk.blue(`PostedChannel: ${postedChannel}`));
+          await say(`Here's latest message of <#${channelId}|${channel.name}> below.`);
+          await say(`*Latest message:* ${message}`);
+        }
+        });
+        
+    } catch(error) {
+      console.log(chalk.red(error));
+      await say("Latest Message couldn't find from the channel.");
+      await say('Access the channel below to make sure whether this chanel existed latest message or not.');
+      channels.forEach(async (channel) => {
+        if(channel.name === channelName) {
+          const channelId = channel.id;
+          await say(`<${channelId}|${channelName}>\nhttps://app.slack.com/client/T05F5GD3ERG/${channelId}`);
+        }
+     });
+    }
+    });          
+    
+>>>>>>> 2a2ddee (Hello)
     app.start();
      
